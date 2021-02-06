36a076becf2f366654180fef0e6b55f66713de86
Abrar Rafid Noor <arn.official@yahoo.com>
2021-02-06 11:47:27 +0600

    created the initial project folder

---
+++ Shahajjokori-SD-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shahajjokori_SD_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
